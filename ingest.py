80d575389f84d6e52d0ad5703fef84a7dd30bd42
valkryhx <283306449@qq.com>
2023-09-01 11:45:24 +0800

    Update ingest.py

--- ingest.py
+++ ingest.py
@@ -122,10 +122,17 @@ def main(device_type):
     logging.info(f"Loading documents from {SOURCE_DIRECTORY}")
     documents = load_documents(SOURCE_DIRECTORY)
     text_documents, python_documents = split_documents(documents)
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+
+    # chunck改小
+    # text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    # python_splitter = RecursiveCharacterTextSplitter.from_language(
+    #     language=Language.PYTHON, chunk_size=880, chunk_overlap=200
+    # )
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
     python_splitter = RecursiveCharacterTextSplitter.from_language(
-        language=Language.PYTHON, chunk_size=880, chunk_overlap=200
+        language=Language.PYTHON, chunk_size=100, chunk_overlap=20
     )
+    
     texts = text_splitter.split_documents(text_documents)
     texts.extend(python_splitter.split_documents(python_documents))
     logging.info(f"Loaded {len(documents)} documents from {SOURCE_DIRECTORY}")
